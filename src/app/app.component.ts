a9d4d1df486b0f93e8b40402686d92399f18b472
Zodiaco1999 <bsbaez023@misena.edu.co>
2024-11-09 13:13:15 -0500

    Create home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,33 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'vaper-app';
+
+  infoItems: InfoItem[] = [
+    {
+      icon: '../assets/images/icono1.png',
+      title: '¿Qué es el vapeo?',
+      description: 'Una guía rápida sobre la definición.'
+    },
+    {
+      icon: '../assets/images/icono2.png',
+      title: 'Efectos del vapeo en la salud',
+      description: 'Conoce las consecuencias del vapeo.'
+    },
+    {
+      icon: '../assets/images/icono3.png',
+      title: 'El vapeo en el entorno académico',
+      description: 'Impacto en las instituciones educativas.'
+    },
+    {
+      icon: '../assets/images/icono4.png',
+      title: 'Agenda tu cita',
+      description: 'Los mejores especialistas que te guiarán para dejar el vapeo.'
+    }
+  ];
+}
+
+type InfoItem = {
+  icon: string;
+  title: string;
+  description: string;
 }
