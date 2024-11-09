eba8d628e1381d3f12666b65e39679217b5c0a10
Zodiaco1999 <bsbaez023@misena.edu.co>
2024-11-09 13:36:21 -0500

    replace images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,22 +13,22 @@ export class AppComponent {
 
   infoItems: InfoItem[] = [
     {
-      icon: '../assets/images/icono1.png',
+      icon: 'assets/images/icono1.png',
       title: '¿Qué es el vapeo?',
       description: 'Una guía rápida sobre la definición.'
     },
     {
-      icon: '../assets/images/icono2.png',
+      icon: 'assets/images/icono2.png',
       title: 'Efectos del vapeo en la salud',
       description: 'Conoce las consecuencias del vapeo.'
     },
     {
-      icon: '../assets/images/icono3.png',
+      icon: 'assets/images/icono3.png',
       title: 'El vapeo en el entorno académico',
       description: 'Impacto en las instituciones educativas.'
     },
     {
-      icon: '../assets/images/icono4.png',
+      icon: 'assets/images/icono4.png',
       title: 'Agenda tu cita',
       description: 'Los mejores especialistas que te guiarán para dejar el vapeo.'
     }
