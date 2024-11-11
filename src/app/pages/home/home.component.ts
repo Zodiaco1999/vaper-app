eea40bc7b7136beebbef93f7d55ce8faf078989a
Zodiaco1999 <bsbaez023@misena.edu.co>
2024-11-11 14:45:18 -0500

    added button install

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -10,7 +10,7 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   infoItems: InfoItem[] = [
     {
       icon: 'assets/images/icono1.png',
@@ -37,6 +37,35 @@ export class HomeComponent {
       link: 'especialistas'
     }
   ];
+
+  deferredPrompt: any;
+  showInstallButton = false;
+
+  ngOnInit() {
+    if (typeof window !== 'undefined') { // Verifica que 'window' esté disponible
+      window.addEventListener('beforeinstallprompt', (event) => {
+        event.preventDefault();
+        this.deferredPrompt = event;
+        this.showInstallButton = true;
+      });
+    }
+  }
+
+  installApp() {
+    // Muestra el prompt de instalación
+    this.deferredPrompt.prompt();
+    // Espera la respuesta del usuario
+    this.deferredPrompt.userChoice.then((choiceResult: any) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('El usuario aceptó la instalación');
+      } else {
+        console.log('El usuario rechazó la instalación');
+      }
+      // Oculta el botón después de mostrar el prompt
+      this.deferredPrompt = null;
+      this.showInstallButton = false;
+    });
+  }
 }
 
 type InfoItem = {
